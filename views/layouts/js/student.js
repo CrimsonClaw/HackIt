039e28e1592c292cae56199973e0e7bcff7e39ef
Pudhiyavan <49871592+CrimsonRyu@users.noreply.github.com>
2020-11-16 18:55:14 +0530

    Delete student.js

--- views/layouts/js/student.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    var elmnt = document.getElementsByTagName("BODY");
-    var y = elmnt.scrollHeight;
-    var x = elmnt.scrollWidth;
-    console.log("H= " + y + "W= " + x);
-});
